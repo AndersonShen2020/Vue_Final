9975efe62bb90ca2c059ec0d6500f16677ea5553
AndersonShen <shen.andersonshen@gmail.com>
2022-03-21 18:39:08 +0800

    fix: 調整背景圖 size

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,7 @@ a {
 }
 
 #app {
-  // background-color: rgba(0, 0, 0, 0.1);
-  // background-blend-mode: multiply;
-  // background-image: url("https://images.unsplash.com/photo-1618105965240-9aa565e73a0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80");
-  width: 100%;
-  height: 100vh;
-  // background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-  //   url("https://images.unsplash.com/photo-1618105965240-9aa565e73a0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80")
-  //     no-repeat 0% 20% / cover;
+  background-size: 100% 100%;
   background: linear-gradient(
       90deg,
       rgba(255, 255, 255, 0.5) 0%,
@@ -34,8 +27,7 @@ a {
       rgba(255, 255, 255, 0.9) 90%,
       rgba(255, 255, 255, 0.5) 100%
     ),
-    url("https://images.unsplash.com/photo-1618105965240-9aa565e73a0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80")
-      no-repeat 0% 20% / cover;
+    url("https://images.unsplash.com/photo-1618105965240-9aa565e73a0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80");
 }
 // 切版格線,切版完後要刪除
 // * {
