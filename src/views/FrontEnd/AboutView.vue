f425bea6b818814969a83f414ff1bcc16ce4bb9e
AndersonShen <shen.andersonshen@gmail.com>
2022-04-20 01:27:25 +0800

    perf: 調整門市頁面

--- src/views/FrontEnd/AboutView.vue
+++ src/views/FrontEnd/AboutView.vue
@@ -12,19 +12,29 @@
     <div class="row mt-3">
       <div class="col-md-6">
         <div class="mt-3 d-flex flex-column">
-          <h2 class="text-primary fw-bold"><i class="bi bi-shop"></i> 門市資訊</h2>
+          <h2 class="text-primary fw-bold">
+            <i class="bi bi-shop"></i> 門市資訊
+          </h2>
           <p class="my-3 h5">
             我們非常高興能聽到來自客戶的任何意見，歡迎隨時聯絡我們，詢問、訂購產品或提供寶貴建議！
           </p>
 
           <div class="m-3 h4 text-primary fw-bold">
-            <p>營業時間： 週一至週六 09：00 AM ~ 19：00 PM</p>
+            <p>
+              營業時間 <i class="bi bi-clock"></i> ： 週一至週六 09：00 AM ~
+              19：00 PM
+            </p>
             <p>
               聯絡地址
               <i class="bi bi-house"></i>
               ：台北市大安區通化街165巷24弄10號
             </p>
-            <p>客服專線 <i class="bi bi-telephone"></i> ：02-2587-6666</p>
+            <p>
+              客服專線 <i class="bi bi-telephone"></i> ：<a
+                href="tel:+886-2-5876666"
+                >02-2587-6666</a
+              >
+            </p>
           </div>
         </div>
       </div>
